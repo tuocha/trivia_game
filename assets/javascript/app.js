ce92800904ea2cbddefca0e2b18a842ac05ef9d9
Cody <kippon47@gmail.com>
2019-06-19 15:55:06 -0400

    improved functionality and added CSS

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,75 +1,137 @@
 $(document).ready(function () {
 
-    //declaring global variables
-    var userPick; 
-    var correctAnswers = 0;
-    var incorrectAnswers = 0;
-    var unanswered = 0;
-    var question = 0;
-    
+    $("#remaining-time").hide();
+    $("#start").on("click", trivia.startGame)
+    $(document).on("click", ".option", trivia.guessChecker);
+})
 
-    //variable to set the time for each question
-    var varTimer = 30;
+var trivia = {
+    correct: 0,
+    incorrect: 0,
+    unanswered: 0,
+    current: 0,
+    timer: 20,
+    timerOn: false,
+    timerId: '',
 
-    //declaring an array of questions objects
-    var questionsArray = [
-        {
-            question: "What is Peter Baelish's nickname?",
-            options: ["The Hound", "Little Lion", "Littlefinger", "The Sword of the Morning"],
-            rightAnswer: 2
-        },
-        {
-            question: "Samwell Tarly's family home is called what?",
-            options: ["Horn Hill", "The Eyrie", "Harrenhall", "Stokeworth"],
-            rightAnswer: 0
+    questions: {
+        q1: "What is Peter Baelish's nickname?",
+        q2: "Samwell Tarly's family home is called what?",
+        q3: "What did Oberyn Martell study at the Citadel?",
+        q4: "Catelyn Stark bore how many children?",
+        q5: "What are the Targaryen colors?",
+        q6: "The Arbor is in which province?",
+        q7: "Jon Show was elected which Lord Commander of the Night's Watch?",
+        q8: "Who is said to have built the Wall?"
+    },
+    options: {
+        q1: ["The Hound", "Little Lion", "Littlefinger", "The Sword of the Morning"],
+        q2: ["Horn Hill", "The Eyrie", "Harrenhall", "Stokeworth"],
+        q3: ["Alchemy", "Poisons", "Swordfighting", "Ravenry"],
+        q4: ["2", "3", "5", "6"],
+        q5: ["Red and gold", "Blue and white", "Red and orange", "Red and black"],
+        q6: ["The Reach", "The Veil", "Dorne", "None, it's in Essos"],
+        q7: ["1000th", "998th", "1001st", "500th"],
+        q8: ["Lann the Clever", "King Aegon I", "The Night's King", "Brann the Builder"]
+    },
+    answers: {
+        q1: "Littlefinger",
+        q2: "Horn Hill",
+        q3: "Poisons",
+        q4: "5",
+        q5: "Red and black",
+        q6: "The Reach",
+        q7: "998th",
+        q8: "Brann the Builder"
+    },
+
+    startGame: function () {
+        trivia.current = 0;
+        trivia.correct = 0;
+        trivia.incorrect = 0;
+        trivia.unanswered = 0;
+        clearInterval(trivia.timerId);
+
+        $("#game").show();
+        $("#results").html('')
+        $("#timer").text(trivia.timer)
+        $("#start").hide();
+        $("#remaining-time").show();
+
+        trivia.nextQuestion();
+    },
+
+    nextQuestion: function () {
+        trivia.timer = 5;
+        $("#timer").text(trivia.timer);
+
+        if (!trivia.timerOn) {
+            trivia.timerId = setInterval(trivia.runTimer, 1000);
         }
-    ];
 
+        var questionFill = Object.values(trivia.questions)[trivia.current];
+        $("#question-display").text(questionFill)
 
-    $("#start").click(function () {
-        $(this).hide();
-        runGame();
-        counter = setInterval(timerFunction, 1000);
-    });
+        var optionsFill = Object.values(trivia.options)[trivia.current];
+        $.each(optionsFill, function (index, key) {
+            $("#choices-display").append($("<button class='option'>" + key + "</button>"));
+        })
+    },
 
-
-    function timerFunction() {
-        varTimer--;
-        if (varTimer <= 0) {
-            clearInterval(counter);
-            return;
+    runTimer: function () {
+        if (trivia.timer > -1 && trivia.current < Object.keys(trivia.questions).length) {
+            $("#timer").text(trivia.timer);
+            trivia.timer--;
         }
-        $("#timer").html("Time remaining: " + "00:" + varTimer + " secs");
+        else if (trivia.timer === -1) {
+            trivia.unanswered++;
+            trivia.resultId = false;
+            clearInterval(trivia.timerId);
+            resultId = setTimeout(trivia.questionRemove, 1000);
+            $("#results").html("<h1> Out of time! The answer was " + Object.values(trivia.answers)[trivia.current] +"</h1>");
+            
+        }
+        else if (trivia.current === Object.keys(trivia.questions).length) {
+            $("#question-display").hide();
+
+            $("#results")
+                .html('<h3>Valar morghulis</h3>' +
+                    '<p>Correct: ' + trivia.correct + '</p>' +
+                    '<p>Incorrect: ' + trivia.incorrect + '</p>' +
+                    '<p>Unaswered: ' + trivia.unanswered + '</p>');
+
+            $("#start").addClass("start-button-after")
+            $("#start").show();
+        }
+    },
+
+    guessChecker: function () {
+        var resultId;
+        var currentAnswer = Object.values(trivia.answers)[trivia.current];
+
+        if ($(this).text() === currentAnswer) {
+            trivia.correct++;
+            clearInterval(trivia.timerId);
+            resultId = setTimeout(trivia.questionRemove, 2000);
+            $("#results").html("<h1>Correct.</h1>");
+        }
+        else {
+            trivia.incorrect++;
+            clearInterval(trivia.timerId);
+            resultId = setTimeout(trivia.questionRemove, 2000);
+            $("#results").html("<h1>Incorrect. The answer was " + Object.values(trivia.answers)[trivia.current] + "</h1>");
+        }
+
+    },
+
+    questionRemove: function () {
+
+        trivia.current++;
+        $(".option").remove();
+        $("#results h1").remove();
+
+        trivia.nextQuestion();
     }
 
-    function runGame() {
-        correctAnswers = 0;
-        incorrectAnswers = 0;
-        unanswered = 0;
+}
 
-        $("#question-display").html(questionsArray[0].question);
-        question++
-            var optionsArr = questionsArray[0].options
-
-          for (let i = 0; i < optionsArr.length; i++) {
-            var button = $('<button>');
-            button.text(optionsArr[i]);
-            button.attr('data-id', i);
-            $('#choices-display').append(button);
-           }
-        
-          }
-
-    $("#choices-display").on("click", "button", function () {
-        userPick = $(this).data("id");
-        var correct = questionsArray[0].rightAnswer;
-        console.log(correct)
-            if (userPick !== correct) {
-                $("#choices-display").text("Nope! That's incorrect.")
-                incorrectAnswers++;
-            } else {
-                $("#choices-display").text("Hurray! That's correct")
-
-            }
-    })
-});
